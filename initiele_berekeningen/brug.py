75dceeca59cedcd14ff81e268f3541057e5a69b4
SamClercky <sam.clercky@gmail.com>
2021-04-21 17:03:02 +0200

    ende van de dag

--- initiele_berekeningen/brug.py
+++ initiele_berekeningen/brug.py
@@ -56,6 +56,24 @@ class BrugInterface:
         I = abs(N)*L**2/(Cte*math.pi**2*E)
 
         return I
+    
+    def calc_piloon_balk(self, x: float, balk1: bool):
+        """
+        Berekenen van balk met stukjes van 0.006m x 0.0318m
+        """
+        h_step = 0.006
+        b_step = 0.034
+        h = h_step
+        b = b_step
+
+        I = lambda h, b: b*h**3/12
+        Pr = self.calc_piloon_I(x, balk1)
+
+        while I(h, b) < Pr:
+            h += h_step
+            b += b_step
+        
+        return h, b
 
     def calc_fitness(self):
         """
@@ -64,11 +82,15 @@ class BrugInterface:
         """
         self.update_data()
         self.calc_reactie_krachten()
-        self.calc_touw_kracht()
-        self.calc_kant()
+        FiA, FiB = self.calc_touw_kracht()
+        Vc, Hc, FiC, Vd, Hd, FiD = self.calc_kant()
         wdN, wdD, wdM = list(zip(*[self.calc_intern_wegdek(x) for x in np.arange(0, 2, 0.1)])) # wd = wegdek
         b1N, b1D, b1M = list(zip(*[self.calc_intern_balk(x, True) for x in np.arange(0, self.h1, 0.01)]))
         b2N, b2D, b2M = list(zip(*[self.calc_intern_balk(x, False) for x in np.arange(0, self.h2, 0.01)]))
+        doorbuiging = [self.calc_doorbuiging_wegdek(x) for x in np.arange(0, 2.0, 0.01)]
+        I1 = [self.calc_piloon_I(x, True) for x in np.arange(0, self.h1, 0.01)]
+        I1 = [self.calc_piloon_I(x, False) for x in np.arange(0, self.h2, 0.01)]
+
         wdN, wdD, wdM = max(np.abs(wdN)), max(np.abs(wdD)), max(np.abs(wdM))
         b1N, b1D, b1M = max(np.abs(b1N)), max(np.abs(b1D)), max(np.abs(b1M))
         b2N, b2D, b2M = max(np.abs(b2N)), max(np.abs(b2D)), max(np.abs(b2M))
